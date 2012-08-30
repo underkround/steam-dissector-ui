1d10b3ea20a55976676dd968de8c7f9f19a712e3
Jussi Rajala <zemm@iki.fi>
2012-08-31 02:23:01 +0300

    First filters fix

--- pub/js/gamegrid/model/game-collection.js
+++ pub/js/gamegrid/model/game-collection.js
@@ -1,11 +1,14 @@
 /**
  *
  * Events:
- *   fetchgames:begin     (status)
- *   fetchgames:done      (status)
- *   fetchgames:tick      (status, id)
- *   fetchgames:success   (status, id)
- *   fetchgames:error     (status, id)
+ *   fetchgames:begin     (status, this)
+ *   fetchgames:done      (status, this)
+ *   fetchgames:tick      (status, id, this)
+ *   fetchgames:success   (status, id, this)
+ *   fetchgames:error     (status, id, this)
+ *   filters:add          (filter, this)
+ *   filters:reset        (this)
+ *   filters:apply        (this)             Signals end of bulk filter modifications
  */
 
 define([
@@ -28,12 +31,39 @@ define([
 		initialize: function() {
 			this.orderReverse = false;
 			this.orderKey = 'name';
-			this.filterCallback = null;
+			this.filters = [];
 		},
 
-		setFilter: function(filter) {
-			this.filterCallback = filter;
+		//
+		// Filters
+		//
+
+		addFilter: function(filter, options) {
+			this.filters.push(filter);
+			if ( ! options || ! options.silent) {
+				this.trigger('filters:add', filter, this);
+			}
 		},
+
+		resetFilters: function(options) {
+			this.filters = [];
+			if ( ! options || ! options.silent) {
+				this.trigger('filters:reset', this);
+			}
+		},
+
+		applyFilters: function() {
+			this.trigger('filters:apply', this);
+			console.log('filters:apply -> using', this.filters);
+		},
+
+		getFilters: function() {
+			return this.filters;
+		},
+
+		//
+		// Sorting (@TODO: refactor)
+		//
 
 		orderByToggle: function(orderKey) {
 			if (orderKey === this.orderKey) {
@@ -57,16 +87,20 @@ define([
 			return _.sortBy(this.models, comparator);
 		},
 
+		//
+		// Games
+		//
+
 		fetchGames: function(idsToAdd) {
 			var self = this,
 				existingIds = this.pluck('id');
 				newIds = _.difference(idsToAdd, existingIds);
 
 			var status = new utils.LoadStatus(newIds, function() {
-				self.trigger('fetchgames:done', status);
+				self.trigger('fetchgames:done', status, self);
 			});
 
-			this.trigger('fetchgames:begin', status);
+			this.trigger('fetchgames:begin', status, self);
 
 			_.each(newIds, function(id) {
 				var game = new GameModel({id: id});
@@ -74,24 +108,26 @@ define([
 					success: function(){
 						var id = game.get('id');
 						status.success(id);
-						self.trigger('fetchgames:success', status, id)
-							.trigger('fetchgames:tick', status, id)
+						self.trigger('fetchgames:success', status, id, self)
+							.trigger('fetchgames:tick', status, id, self)
 							.add(game);
 					},
 					error: function(){
 						var id = game.get('id');
 						status.error(id);
-						self.trigger('fetchgames:error', status, id)
-							.trigger('fetchgames:tick', status, id);
+						self.trigger('fetchgames:error', status, id, self)
+							.trigger('fetchgames:tick', status, id, self);
 					}
 				})
 			});
 		},
 
 		getFiltered: function() {
-			return (this.filterCallback)
-				? this.filter(this.filterCallback)
-				: this.models;
+			var chain = _.chain(this.models);
+			_.each(this.filters, function(filter) {
+				chain = chain.filter(filter);
+			});
+			return chain.value();
 		},
 
 		filteredToJSON: function(options) {
